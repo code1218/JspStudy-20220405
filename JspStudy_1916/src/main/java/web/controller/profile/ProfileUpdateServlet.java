faf0f45f6e9dfa97bbefa256d45b4ae0dbe0516b
ITPS <ITPS@gmail.com>
2022-04-14 11:48:16 +0900

    서블릿 컨텍스트 설정

--- JspStudy_1916/src/main/java/web/controller/profile/ProfileUpdateServlet.java
+++ JspStudy_1916/src/main/java/web/controller/profile/ProfileUpdateServlet.java
@@ -1,15 +1,29 @@
 package web.controller.profile;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import repository.UserDao;
+import web.service.ProfileService;
+import web.service.ProfileServiceImpl;
+
 @WebServlet("/profile/update")
 public class ProfileUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ProfileService profileService;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		ServletContext servletContext = config.getServletContext();
+		profileService = new ProfileServiceImpl((UserDao)servletContext.getAttribute("userDao"));
+	}
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/profile/profile-update.jsp").forward(request, response);
